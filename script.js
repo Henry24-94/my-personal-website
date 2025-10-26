a4e5743afbd6029593d21929baa54971ea4f2774
Henry <solomonfentaw884@gmail.com>
2025-10-26 21:16:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
